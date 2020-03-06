6a76982a0b516c0e2af2a449994d08fe3fe5b03f
Sethmichael01 <49845849+Sethmichael01@users.noreply.github.com>
2020-03-06 16:31:00 +0100

    Fixed Log out redirect to action page

---
+++ dotNetNews/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using dotNetNews.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(dotNetNews.Areas.Identity.IdentityHostingStartup))]
+namespace dotNetNews.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
